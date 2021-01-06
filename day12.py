15819696ca0b9d0233269a4cc512acd9ceb177a5
Alberto Re <alberto.re@gmail.com>
2021-01-06 15:18:45 +0100

    Day 12 solution (part 2)

--- day12.py
+++ day12.py
@@ -2,23 +2,78 @@
 # --- Day 12: Rain Risk ---
 
 from typing import Tuple
+from math import cos, sin, radians
+
+
+DIRECTIONS = ["N", "W", "S", "E"]
+
+
+class Position:
+
+    """A class representing a pair of x, y coordinates"""
+
+    def __init__(self, x: int = 0, y: int = 0) -> None:
+        self._x = x
+        self._y = y
+
+    def move(self, direction: str, units: int) -> None:
+        assert direction in DIRECTIONS
+        if direction == "N":
+            self._y += units
+        elif direction == "S":
+            self._y -= units
+        elif direction == "E":
+            self._x += units
+        elif direction == "W":
+            self._x -= units
+
+    def move_towards(self, waypoint: "Position", units: int) -> None:
+        relx, rely = units * waypoint.x, units * waypoint.y
+        self._x += relx
+        self._y += rely
+
+    def rotate(self, direction: str, degrees: int) -> None:
+        assert degrees % 90 == 0
+        assert direction in ["L", "R"]
+        theta = radians(90)
+        for _ in range(degrees // 90):
+            (x, y) = self._x, self._y
+            if direction == "R":
+                self._x = x * cos(-theta) - y * sin(-theta)
+                self._y = x * sin(-theta) + y * cos(-theta)
+            if direction == "L":
+                self._x = x * cos(theta) - y * sin(theta)
+                self._y = x * sin(theta) + y * cos(theta)
+
+    def manhattan(self, other: "Position") -> int:
+        return round(abs(self.x - other.x) + abs(self.y - other.y))
+
+    @property
+    def x(self) -> int:
+        return self._x
+
+    @property
+    def y(self) -> int:
+        return self._y
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, Position):
+            return self.x == other.x and self.y == other.y
+        if isinstance(other, tuple):
+            return self.x == other[0] and self.y == other[1]
+        return False
 
 
 class Ship:
-    def __init__(self) -> None:
-        self._coordinates = (0, 0)
-        self._orientation = 90
+    def __init__(self, position: Position) -> None:
+        self._position = Position(position.x, position.y)
+        self._orientation = 0
 
     def move(self, direction: str, units: int) -> None:
-        assert direction in ["N", "W", "S", "E"]
-        if direction == "N":
-            self._coordinates = (self._coordinates[0], self._coordinates[1] + units)
-        elif direction == "S":
-            self._coordinates = (self._coordinates[0], self._coordinates[1] - units)
-        elif direction == "W":
-            self._coordinates = (self._coordinates[0] - units, self._coordinates[1])
-        elif direction == "E":
-            self._coordinates = (self._coordinates[0] + units, self._coordinates[1])
+        self._position.move(direction, units)
+
+    def move_towards(self, waypoint: "Position", units: int) -> None:
+        self._position.move_towards(waypoint, units)
 
     def forward(self, units: int) -> None:
         self.move(self.cardinal, units)
@@ -27,14 +82,14 @@ class Ship:
         assert degrees % 90 == 0
         assert direction in ["L", "R"]
         if direction == "R":
-            self._orientation += degrees
-        elif direction == "L":
             self._orientation -= degrees
+        else:
+            self._orientation += degrees
         self._orientation %= 360
 
     @property
-    def coordinates(self) -> Tuple[int, int]:
-        return self._coordinates
+    def position(self) -> Position:
+        return self._position
 
     @property
     def degrees(self) -> int:
@@ -43,27 +98,32 @@ class Ship:
     @property
     def cardinal(self) -> int:
         if self._orientation == 0:
-            return "N"
-        elif self._orientation == 90:
             return "E"
+        elif self._orientation == 90:
+            return "N"
         elif self._orientation == 180:
-            return "S"
-        elif self._orientation == 270:
             return "W"
+        elif self._orientation == 270:
+            return "S"
 
 
-def navigate(ship: Ship, instruction: str) -> None:
+def navigate(ship: Ship, instruction: str, waypoint: Position = None) -> None:
     (action, value) = (instruction[0], int(instruction[1:]))
     if action in ["N", "S", "W", "E"]:
-        ship.move(action, value)
+        if waypoint:
+            waypoint.move(action, value)
+        else:
+            ship.move(action, value)
     if action in ["L", "R"]:
-        ship.rotate(action, value)
+        if waypoint:
+            waypoint.rotate(action, value)
+        else:
+            ship.rotate(action, value)
     if action in ["F"]:
-        ship.forward(value)
-
-
-def manhattan(start: Tuple[int, int], end: Tuple[int, int]) -> int:
-    return abs(start[0] - end[0]) + abs(start[1] - end[1])
+        if waypoint:
+            ship.move_towards(waypoint, value)
+        else:
+            ship.forward(value)
 
 
 if __name__ == "__main__":
@@ -71,8 +131,17 @@ if __name__ == "__main__":
     with open("input/day12.txt") as f:
         data = f.read()
 
-    ship = Ship()
+    starting_position = Position(0, 0)
+    ship = Ship(starting_position)
     for i in data.split("\n")[:-1]:
         navigate(ship, i)
 
-    print("part1 solution:", manhattan((0, 0), ship.coordinates))
+    print("part1 solution:", ship.position.manhattan(starting_position))
+
+    starting_position = Position(0, 0)
+    waypoint = Position(10, 1)
+    ship = Ship(starting_position)
+    for i in data.split("\n")[:-1]:
+        navigate(ship, i, waypoint)
+
+    print("part2 solution:", ship.position.manhattan(starting_position))
--- tests/test_day12.py
+++ tests/test_day12.py
@@ -1,4 +1,4 @@
-from day12 import Ship, manhattan, navigate
+from day12 import Position, Ship, navigate
 
 
 PART1_INSTR = """\
@@ -10,59 +10,116 @@ F11
 """
 
 
+def test_position_move():
+    pos = Position(0, 0)
+    assert pos == (0, 0)
+    pos.move("N", 30)
+    assert pos == (0, 30)
+    pos.move("E", 40)
+    assert pos == (40, 30)
+    pos.move("S", 20)
+    assert pos == (40, 10)
+    pos.move("W", 75)
+    assert pos == (-35, 10)
+
+
+def test_position_move_towards():
+    pos = Position(0, 0)
+    assert pos == (0, 0)
+    pos.move_towards(Position(10, 5), 30)
+    assert pos == (300, 150)
+    pos.move_towards(Position(-10, -5), 30)
+    assert pos == (0, 0)
+
+
+def test_position_rotate_right():
+    pos = Position(7, 7)
+    pos.rotate("R", 90)
+    assert pos == (7, -7)
+    pos.rotate("R", 90)
+    assert pos == (-7, -7)
+    pos.rotate("R", 90)
+    assert pos == (-7, 7)
+    pos.rotate("R", 90)
+    assert pos == (7, 7)
+
+
+def test_position_rotate_left():
+    pos = Position(7, 7)
+    pos.rotate("L", 90)
+    assert pos == (-7, 7)
+    pos.rotate("L", 90)
+    assert pos == (-7, -7)
+    pos.rotate("L", 90)
+    assert pos == (7, -7)
+    pos.rotate("L", 90)
+    assert pos == (7, 7)
+
+
+def test_position_manhattan():
+    pos = Position(0, 0)
+    assert pos.manhattan(Position(10, -5)) == 15
+    assert pos.manhattan(Position(12, 53)) == 65
+    assert pos.manhattan(Position(-7, -4)) == 11
+
+
 def test_ship_starting_coords():
-    ship = Ship()
-    assert ship.coordinates == (0, 0)
-    assert ship.degrees == 90
+    ship = Ship(Position(0, 0))
+    assert ship.position == (0, 0)
+    assert ship.degrees == 0
 
 
 def test_ship_rotation_right():
-    ship = Ship()
+    ship = Ship(Position(0, 0))
+    ship.rotate("R", 90)
+    assert ship.degrees == 270
     ship.rotate("R", 90)
     assert ship.degrees == 180
     ship.rotate("R", 90)
-    assert ship.degrees == 270
-    ship.rotate("R", 90)
-    assert ship.degrees == 0
+    assert ship.degrees == 90
     ship.rotate("R", 270)
-    assert ship.degrees == 270
+    assert ship.degrees == 180
 
 
 def test_ship_rotation_left():
-    ship = Ship()
+    ship = Ship(Position(0, 0))
     ship.rotate("L", 90)
-    assert ship.degrees == 0
-    ship.rotate("L", 90)
-    assert ship.degrees == 270
+    assert ship.degrees == 90
     ship.rotate("L", 90)
     assert ship.degrees == 180
     ship.rotate("L", 90)
-    assert ship.degrees == 90
-    ship.rotate("L", 180)
     assert ship.degrees == 270
+    ship.rotate("L", 90)
+    assert ship.degrees == 0
+    ship.rotate("L", 180)
+    assert ship.degrees == 180
 
 
 def test_ship_forward():
-    ship = Ship()
+    ship = Ship(Position(0, 0))
     ship.forward(10)
-    assert ship.coordinates == (10, 0)
+    assert ship.position == (10, 0)
     ship.rotate("R", 180)
-    assert ship.coordinates == (10, 0)
+    assert ship.position == (10, 0)
     ship.forward(10)
-    assert ship.coordinates == (0, 0)
+    assert ship.position == (0, 0)
     ship.forward(30)
-    assert ship.coordinates == (-30, 0)
+    assert ship.position == (-30, 0)
 
 
-def test_manhattan():
-    assert manhattan((0, 0), (5, 5)) == 10
-    assert manhattan((0, 0), (-3, 5)) == 8
-    assert manhattan((0, 0), (17, -8)) == 25
-
-
-def test_navigate():
-    ship = Ship()
+def test_navigate_part1():
+    starting_position = Position(0, 0)
+    ship = Ship(starting_position)
     for i in PART1_INSTR.split("\n")[:-1]:
         navigate(ship, i)
-    assert ship.coordinates == (17, -8)
-    assert manhattan((0, 0), ship.coordinates) == 25
+    assert ship.position == (17, -8)
+    assert ship.position.manhattan(starting_position) == 25
+
+def test_navigate_part2():
+    starting_position = Position(0, 0)
+    waypoint = Position(10, 1)
+    ship = Ship(starting_position)
+    for i in PART1_INSTR.split("\n")[:-1]:
+        navigate(ship, i, waypoint)
+    assert ship.position == (214, -72)
+    assert ship.position.manhattan(starting_position) == 286
